7592f52bcd14fb856496fb5169492053ca363158
Charles Cabergs <charles.cabergs@colruytgroup.com>
2022-06-07 19:07:34 +0200

    Added Tetrimino bitwise representation

--- app/Main.hs
+++ app/Main.hs
@@ -4,11 +4,16 @@ import Tetrimino
 import Solve
 
 
+-- main :: IO ()
+-- main = do
+--     content <- getContents
+--     case readTetriminos content of
+--         Left err -> putStrLn $ "Error: Invalid tetriminos format: " ++ err
+--         Right ts -> if length ts < 1 || length ts > 26
+--                     then putStrLn $ "Error: The number of tetriminos in the file must be between 1 and 26"
+--                     else putStr $ showSolve $ solve ts
+
+
 main :: IO ()
-main = do
-    content <- getContents
-    case readTetriminos content of
-        Left err -> putStrLn $ "Error: Invalid tetriminos format: " ++ err
-        Right ts -> if length ts < 1 || length ts > 26
-                    then putStrLn $ "Error: The number of tetriminos in the file must be between 1 and 26"
-                    else putStr $ showSolve $ solve ts
+main = print $ normalize $ shift 3 3 $ scale 10 $ fromPositions $ Positions [(1, 2), (2, 2)]
+-- main = print $ toPositions $ Tetrimino 4 7
--- src/Tetrimino.hs
+++ src/Tetrimino.hs
@@ -1,5 +1,9 @@
 module Tetrimino
     ( Tetrimino(..)
+    , Positions(..)
+    , fromPositions
+    , toPositions
+    , scale
     , readTetriminoFromLines
     , readTetriminos
     , shift
@@ -11,28 +15,98 @@ module Tetrimino
     ) where
 
 
+import Data.Bits (shiftR, shiftL, (.|.), (.&.)) -- TODO: try unsafeShiftL
 import Data.List (intersect, elemIndices, dropWhileEnd)
 import Data.List.Split (splitOn)
 import Data.Char (isSpace)
 
+import Debug.Trace
 
-type Positions = [(Int, Int)]
-newtype Tetrimino = Tetrimino { getPositions :: Positions }
+newtype Positions = Positions { getPositions :: [(Int, Int)] }
+data Tetrimino = Tetrimino { getSize :: Int, getBits :: Int }
 
-readTetrimino :: String -> Either String Tetrimino
-readTetrimino = readTetriminoFromLines . lines
+
+positionsSize :: Positions -> Int
+positionsSize (Positions xs) = 1 + (maximum $ map (uncurry max) xs)
+
+
+toPositions :: Tetrimino -> Positions
+toPositions (Tetrimino size b) = Positions
+                                     [(y, x)
+                                     | y <- indices
+                                     , x <- indices
+                                     , ((positionToBits size (y, x)) .&. b) /= 0
+                                     ]
+    where indices = [0..(size - 1)]
+
+
+fromPositionsSize :: Int -> Positions -> Tetrimino
+fromPositionsSize size pos@(Positions xs) =
+    Tetrimino size $ foldl1 (.|.) $ map (positionToBits size) xs
+
+fromPositions :: Positions -> Tetrimino
+fromPositions pos = fromPositionsSize (positionsSize pos) pos
+
+
+positionToBits :: Int -> (Int, Int) -> Int
+positionToBits size (y, x) = (1 `shiftL` x) `shiftL` (size * y)
+
+
+scale :: Int -> Tetrimino -> Tetrimino
+scale size t = fromPositionsSize (size + 1) (toPositions t)
+
+
+shift :: Int -> Int -> Tetrimino -> Tetrimino
+shift 0 x (Tetrimino size b)
+    | x > 0 = Tetrimino size (b `shiftL` x)
+    | x < 0 = Tetrimino size (b `shiftR` (-x))
+shift y 0 (Tetrimino size b)
+    | y > 0 = Tetrimino size (b `shiftL` (y * size))
+    | y < 0 = Tetrimino size (b `shiftR` ((-y) * size))
+shift y x t = shift y 0 $ shift 0 x t
+
+
+normalizeX :: Tetrimino -> Tetrimino
+normalizeX t@(Tetrimino size b)
+    | leftBarMask .&. b == 0 = normalizeX $ shift 0 (-1) t
+    | otherwise = t
+    where leftBarMask = foldl1 (.|.) [positionToBits size (y, 0) | y <- [0..(size - 1)]]
+
+
+normalizeY :: Tetrimino -> Tetrimino
+normalizeY t@(Tetrimino size b)
+    | topBarMask .&. b == 0 = normalizeY $ shift (-1) 0 t
+    | otherwise = t
+    where topBarMask = foldl1 (.|.) [positionToBits size (0, x) | x <- [0..(size - 1)]]
+
+normalize :: Tetrimino -> Tetrimino
+normalize = normalizeY . normalizeX
+
+
+overlap :: [Tetrimino] -> Tetrimino -> Bool
+overlap ts (Tetrimino size b) = mask .&. b /= 0
+    where mask = foldl1 (.|.) $ map getBits ts
+
+
+instance Eq Tetrimino where
+    t1 == t2 = let (Tetrimino _ b1) = normalize t1
+                   (Tetrimino _ b2) = normalize t2
+               in b1 == b2
+
 
 readTetriminoFromLines :: [String] -> Either String Tetrimino
 readTetriminoFromLines ls
     | length ls /= 4                   = Left "Has to be 4 lines long"
     | any ((/= 4) . length) ls         = Left "Each line has to be 4 characters long"
     | any (any (flip notElem "#.")) ls = Left "Can only contain '#' and '.'"
-    | otherwise = if isValid parsedTetrimino
-                    then Right $ normalize parsedTetrimino
+    | otherwise = if isValid parsedPos
+                    then Right $ normalize $ fromPositions parsedPos
                     else Left "Has to contain 4 cell attached to each other"
-    where parsedTetrimino = Tetrimino $
+    where parsedPos = Positions $
             map (\i -> (i `div` 4, i `mod` 4)) $ elemIndices '#' $ concat ls
 
+readTetrimino :: String -> Either String Tetrimino
+readTetrimino = readTetriminoFromLines . lines
 
 readTetriminos :: String -> Either String [Tetrimino]
 readTetriminos s = let s' = trimSpaces s
@@ -40,21 +114,8 @@ readTetriminos s = let s' = trimSpaces s
                    in  sequence $ map readTetriminoFromLines $ splitOn [""] ls
     where trimSpaces = dropWhile isSpace . dropWhileEnd isSpace
 
-
-showWithSizeAndId :: Int -> Char -> Tetrimino -> String
-showWithSizeAndId squareSize c (Tetrimino pos) =
-    unlines [
-        [if (y, x) `elem` pos then c else '.' | x <- [0..squareSize]]
-        | y <- [0..squareSize]
-    ]
-
-instance Show Tetrimino where
-    show t@(Tetrimino pos) = showWithSizeAndId squareSize '#' t
-        where squareSize = maximum (map (uncurry max) pos)
-
-
-isValid :: Tetrimino -> Bool
-isValid (Tetrimino pos) = length pos == 4 && all isConnectedPos pos
+isValid :: Positions -> Bool
+isValid (Positions pos) = length pos == 4 && all isConnectedPos pos
     where isConnectedPos (y, x) = any (flip elem shifts) pos
                 where shifts = [ (y + 1, x)
                                , (y - 1, x)
@@ -63,27 +124,15 @@ isValid (Tetrimino pos) = length pos == 4 && all isConnectedPos pos
                                ]
 
 
-shift :: Int -> Int -> Tetrimino -> Tetrimino
-shift y x (Tetrimino t) = Tetrimino $ map (\(y', x') -> (y' + y, x' + x)) t
+showWithSizeAndId :: Int -> Char -> Positions -> String
+showWithSizeAndId squareSize c (Positions pos) =
+    unlines [
+        [if (y, x) `elem` pos then c else '.' | x <- [0..(squareSize - 1)]]
+        | y <- [0..(squareSize - 1)]
+    ]
 
-normalizeY :: Tetrimino -> Tetrimino
-normalizeY t@(Tetrimino pos)
-    | all ((/= 0) . fst) pos = normalizeY $ shift (-1) 0 t
-    | otherwise = t
+instance Show Tetrimino where
+    show = show . toPositions
 
-normalizeX :: Tetrimino -> Tetrimino
-normalizeX t@(Tetrimino pos)
-    | all ((/= 0) . snd) pos = normalizeX $ shift 0 (-1) t
-    | otherwise = t
-
-normalize :: Tetrimino -> Tetrimino
-normalize = normalizeY . normalizeX
-
-overlap :: [Tetrimino] -> Tetrimino -> Bool
-overlap ts (Tetrimino tPos) = any ((/= 0) . length . (intersect tPos)) tsPos
-    where tsPos = map getPositions ts
-
-
-instance Eq Tetrimino where
-    t1 == t2 = normalizePos t1 == normalizePos t2
-        where normalizePos =  getPositions .normalize
+instance Show Positions where
+    show pos = showWithSizeAndId (positionsSize pos) '#' pos
